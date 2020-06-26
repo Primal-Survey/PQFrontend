6d0f153e20c829341f27ebf054e7bcf41aac4f07
wjonke <wesjonke@gmail.com>
2020-06-26 13:40:02 -0400

    boilerplate results text put in results page, set min height to 75vh

--- src/Components/Body/Results/Results.js
+++ src/Components/Body/Results/Results.js
@@ -1,17 +1,22 @@
 import React from "react";
 import Fade from "react-reveal/Fade";
+import styled from "styled-components";
+
 import DataVisual from "./DataVisual";
 import ResultVisual from "./ResultVisual";
 
 function Results(surveyInfo) {
   return (
     <Fade>
-      <div>
+      <Container>
         <DataVisual surveyInfo={surveyInfo} />
         <ResultVisual surveyInfo={surveyInfo} />
-      </div>
+      </Container>
     </Fade>
   );
 }
-
+const Container = styled.div`
+  /* padding-bottom: 25px; */
+  min-height: 75vh;
+`;
 export default Results;
