e239afcf211913da2774b3e6e0614a275f9bf576
JHaydenDev <you@example.com>
2020-06-30 13:07:58 -0500

    Styling

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -12,98 +12,104 @@ import HeaderBanner from "./../../Assets/Images/HeaderBanner.jpg";
 function Footer() {
   return (
     <BackgroundLayer>
-    <Foot className="page-footer N/A transparent">
-      <Content className="container">
-        <div className="row ">
-          <LeftBox className="col l6 s12">
-            <Section>
-              <div>
-                <h5 className="teal-text">
-                  Captures insights and advice that everyone can use to thrive
-                  in the constantly evolving workplace.
-                </h5>
-                <H6>
-                  — DANIEL H. PINK, NEW YORK TIMES BESTSELLING AUTHOR OF WHEN,
-                  DRIVE, AND TO SELL IS HUMAN.
-                </H6>
-              </div>
-              <ImgBox>
+      <Foot className="page-footer N/A transparent">
+        <Content className="container">
+          <div className="row ">
+            <LeftBox className="col l6 s12">
+              <Section>
                 <div>
-                  <Img2 src={Marco} alt="Primitive Book" />
+                  <h5 className="teal-text">
+                    Captures insights and advice that everyone can use to thrive
+                    in the constantly evolving workplace.
+                  </h5>
+                  <H6>
+                    — DANIEL H. PINK, NEW YORK TIMES BESTSELLING AUTHOR OF WHEN,
+                    DRIVE, AND TO SELL IS HUMAN.
+                  </H6>
                 </div>
+                <ImgBox>
+                  <div>
+                    <Img2 src={Marco} alt="Primitive Book" />
+                  </div>
+                </ImgBox>
+              </Section>
+            </LeftBox>
+
+            {/* -----------------  right links ---------------------------*/}
+            <RightBox className="col l6  s12">
+              <h5 className="teal-text">
+                PURCHASE THE BOOK AT YOUR FAVORITE RETAILER
+              </h5>
+
+              <ImgBox className="col s12 m12 l12 ">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://primitive-book.squarespace.com/#purchase-section"
+                >
+                  <div className="col s6 m3 l3 ">
+                    <Img2 src={BookDown} alt="Primitive Book" />
+                  </div>
+                </a>
               </ImgBox>
-            </Section>
-          </LeftBox>
+
+              <ImgBox className="col s12 m12 l12 ">
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.amazon.com/Primitive-Tapping-Primal-Powering-Successful/dp/0316530379/"
+                >
+                  <div className="col s6 m3 l3 ">
+                    <Img src={Amazon3} alt="" />
+                  </div>
+                </a>
+
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.booksamillion.com/p/Primitive/Marco-Greenberg/9780316530378"
+                >
+                  <div className="col s6 m3 l3 ">
+                    <Img src={Bam2} alt="" />
+                  </div>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.barnesandnoble.com/w/primitive-marco-greenberg/1136270724?ean=9780316530378"
+                >
+                  <div className="col s6 m3 l3 ">
+                    <Img src={BN2} alt="" />
+                  </div>
+                </a>
+                <a
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  href="https://www.indiebound.org/book/9780316530378"
+                >
+                  <div className="col s6 m3 l3 ">
+                    <Img src={IndieBound} alt="" />
+                  </div>
+                </a>
+              </ImgBox>
+            </RightBox>
+          </div>
 
           {/* -----------------  right links ---------------------------*/}
-          <RightBox className="col l6  s12">
-            <h5 className="teal-text">
-              PURCHASE THE BOOK AT YOUR FAVORITE RETAILER
-            </h5>
+        </Content>
 
-            <ImgBox className="col s12 m12 l12 ">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://primitive-book.squarespace.com/#purchase-section"
-              >
-                <div className="col s6 m3 l3 ">
-                  <Img2 src={BookDown} alt="Primitive Book" />
-                </div>
-              </a>
-            </ImgBox>
-
-            <ImgBox className="col s12 m12 l12 ">
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.amazon.com/Primitive-Tapping-Primal-Powering-Successful/dp/0316530379/"
-              >
-                <div className="col s6 m3 l3 ">
-                  <Img src={Amazon3} alt="" />
-                </div>
-              </a>
-
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.booksamillion.com/p/Primitive/Marco-Greenberg/9780316530378"
-              >
-                <div className="col s6 m3 l3 ">
-                  <Img src={Bam2} alt="" />
-                </div>
-              </a>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.barnesandnoble.com/w/primitive-marco-greenberg/1136270724?ean=9780316530378"
-              >
-                <div className="col s6 m3 l3 ">
-                  <Img src={BN2} alt="" />
-                </div>
-              </a>
-              <a
-                target="_blank"
-                rel="noopener noreferrer"
-                href="https://www.indiebound.org/book/9780316530378"
-              >
-                <div className="col s6 m3 l3 ">
-                  <Img src={IndieBound} alt="" />
-                </div>
-              </a>
-            </ImgBox>
-          </RightBox>
+        <div
+          className="footer-copyright N/A transparent"
+          style={{
+            border: "none",
+            boxShadow: "none",
+          }}
+        >
+          <div className="container">
+            © 2020 Marco Greenberg. All Rights Reserved.
+          </div>
         </div>
-
-        {/* -----------------  right links ---------------------------*/}
-      </Content>
-
-      <div className="footer-copyright">
-        <div className="container">
-          © 2020 Marco Greenberg. All Rights Reserved.
-        </div>
-      </div>
-    </Foot>
+      </Foot>
     </BackgroundLayer>
   );
 }
@@ -113,7 +119,7 @@ const Foot = styled.footer`
   /* justify-content: center; */
   align-items: center;
   justify-content: space-evenly;
-  min-height: 50vh;
+  min-height: 100vh;
 `;
 
 const Content = styled.div`
@@ -169,6 +175,7 @@ const Img = styled.img`
 const Img2 = styled.img`
   max-width: 17vw;
   height: auto;
+  border-radius: 100%;
   /* border: 1px solid red; */
 `;
 
@@ -188,6 +195,5 @@ const BackgroundLayer = styled.div`
   min-height: 20vh;
   margin: auto;
 `;
-
 
 export default Footer;
